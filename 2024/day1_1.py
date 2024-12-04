f54667b49b9e286f264a9dc9c4c1bbbfb3fd6f58
Vinod Menon <vinumenon@gmail.com>
2024-12-04 14:06:57 -0500

    2024 day1 added

--- 2024/day1_1.py
+++ 2024/day1_1.py
@@ -1,9 +1,25 @@
 import os
 
-def func():
+def solve():
     with open(os.path.dirname(os.path.realpath(__file__)) + "/day1.txt") as f:
-        data = f.read()  
+        data = f.read().splitlines()
+
+    list1 = []
+    list2 = []
+    for line in data:
+        line = line.replace("   ", ",")
+        list1.append(line.split(",")[0])
+        list2.append(line.split(",")[1])
+    
+    list1.sort()
+    list2.sort()
+    output = 0
+
+    for i in range(0, len(list1)):
+        output = output + abs(int(list1[i]) - int(list2[i]))
+
+    return output
 
 if __name__ == "__main__":
-    func()
+    print(solve())
     
---
+++ 2024/day1_2.py
@@ -0,0 +1,28 @@
+import os
+
+def solve():
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/day1.txt") as f:
+        data = f.read().splitlines()    
+    
+    col1 = []
+    col2 = {}
+    for line in data:
+        line = line.replace("   ", ",")
+        key = int(line.split(",")[0])
+        value = int(line.split(",")[1])
+        
+        col1.append(key)
+        if value in col2:
+            col2[value] = col2[value] + 1
+        else:
+            col2[value] = 1
+
+    output = 0
+    for num in col1:
+        if num in col2:
+            output = output + num * col2[num]    
+
+    return output
+
+if __name__ == "__main__":
+    print(solve())
