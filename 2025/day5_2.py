6b2075ad0f92e5eaa92772b9197882a21fb8a9e9
vinmen <vinumenon@gmail.com>
2025-12-07 00:19:16 -0500

    Added day 5

--- 2025/day5_2.py
+++ 2025/day5_2.py
@@ -1,43 +1,45 @@
 
 import os
 
+def merge_ranges(ranges):
+    for i in range(0, len(ranges) - 1):                   
+        first_range_start = int(ranges[i].split("-")[0])
+        first_range_end = int(ranges[i].split("-")[1])
+        second_range_start = int(ranges[i + 1].split("-")[0])
+        second_range_end = int(ranges[i + 1].split("-")[1])
+
+        if second_range_start <= first_range_end and second_range_end >= first_range_end:
+            ranges[i] = str(first_range_start) + '-' + str(second_range_end)
+            del ranges[i+1]
+            return merge_ranges(ranges)
+        if second_range_start < first_range_end and second_range_end < first_range_end:            
+            del ranges[i+1]
+            return merge_ranges(ranges)
+        
+    return ranges
+
 def solve():
     with open(os.path.dirname(os.path.realpath(__file__)) + "/day5.txt") as f:
-        data = f.read().splitlines()    
+        data = f.read().splitlines()
     
-    ranges = []
-    for line in data:
-        if "-" in line:
-            start = int(line.split("-")[0])
-            end = int(line.split("-")[1])
-            count = len(ranges)            
 
-            if count > 0:
-                min = ranges[0]
-                max = ranges[count - 1]
-                
-                if start >= min and end <= max:
-                    continue
-                elif start < min and end > max:
-                    ranges.clear()
-                    for i in range(start, end + 1):
-                        ranges.append(i)
-                elif start < min and start < max and end < max:
-                    for j in range(start, min):
-                        ranges.append(j)
-                elif start > min and start < max and end > max:
-                    for k in range(max, end + 1):
-                        ranges.append(k)
-                elif start < min and end < min or end > max and start > max:
-                    for l in range(start, end + 1):
-                        ranges.append(l) 
-            else:
-                for m in range(start, end + 1):
-                    ranges.append(m)
+    for i in range(0, len(data) - 1):
+        for j in range(i + 1, len(data)):
+            num1 = int(data[i].split("-")[0])
+            num2 = int(data[j].split("-")[0])
 
-            ranges.sort()
+            if num1 > num2:
+                temp = data[i]
+                data[i] = data[j]
+                data[j] = temp        
 
-    return len(ranges)   
+    ranges = merge_ranges(data)
+
+    total = 0
+    for item in ranges:
+        total = total + int(item.split("-")[1]) -  int(item.split("-")[0]) + 1
+
+    return total
 
 if __name__ == "__main__":
     print(solve())
