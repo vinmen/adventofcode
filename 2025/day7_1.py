eaf85f1bcd12b0acdd08336913e3d510819a664b
Vinod Menon <vinumenon@gmail.com>
2025-12-09 16:40:03 -0500

    Started day 7

---
+++ 2025/day7_1.py
@@ -0,0 +1,40 @@
+import os
+from collections import deque
+
+def solve():
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/day7.txt") as f:
+        data = f.read().splitlines()
+    
+    grid = []
+    for line in data:
+        grid.append(list(line))
+
+    q = deque()
+    #start_list = []
+    for j in range(0, len(grid[0])):
+        if grid[0][j] == 'S':
+            #start_list.append((0, j))
+            q.append((0, j))
+            break
+    
+    splits = set()
+    while(len(q) > 0):       
+        (row, col) = q.popleft()           
+
+        for m in range(row, len(grid)):
+            if grid[m][col] == '^':
+                splits.add((m, col))
+                #q.popleft()    
+                if (m, col - 1) not in q:            
+                    q.append((m, col - 1))
+                if (m, col + 1) not in q:
+                    q.append((m, col + 1))
+                #start_list.remove((row, col))                   
+                #start_list.append((m, col - 1))
+                #start_list.append((m, col + 1))
+                break           
+
+    return len(splits)
+
+if __name__ == "__main__":
+    print(solve())
